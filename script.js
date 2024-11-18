8c60af5ef4d11e3d52d3e92e3d3f3f568aee05c1
Thecoder098 <reyanshdusanapudi@gmail.com>
2024-11-18 19:35:43 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Function to display more information when the "Learn More" button is clicked
+function learnMore() {
+    alert("Learn more about our projects and how we are addressing climate change through innovation and sustainable practices.");
+}
